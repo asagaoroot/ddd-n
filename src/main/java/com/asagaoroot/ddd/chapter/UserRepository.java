5e0a83f256490d05260abf52b346fb5fb7d9a2eb
mogmog1280@gmail.com <mogmog1280@gmail.com>
2025-01-01 16:51:54 +0900

    list 5.1x

--- src/main/java/com/asagaoroot/ddd/chapter/UserRepository.java
+++ src/main/java/com/asagaoroot/ddd/chapter/UserRepository.java
@@ -5,11 +5,15 @@ public class UserRepository implements IUserRepository {
     @Override
     public void save(User user) {
         // save dummy
+//        Connection con = ...
+        System.out.println("Saving user: " + user);
     }
 
     @Override
     public User find(UserName userName) {
         // find dummy
+//        Connection con = ...
+        System.out.println("Find userName: " + userName);
         return new User(userName);
     }
 }
