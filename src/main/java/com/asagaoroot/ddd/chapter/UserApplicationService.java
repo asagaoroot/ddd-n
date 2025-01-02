4bca6acd0c4e78f12774225870a765e6192f7754
mogmog1280@gmail.com <mogmog1280@gmail.com>
2025-01-02 16:00:46 +0900

    list 6.15

--- src/main/java/com/asagaoroot/ddd/chapter/UserApplicationService.java
+++ src/main/java/com/asagaoroot/ddd/chapter/UserApplicationService.java
@@ -1,12 +1,22 @@
 package com.asagaoroot.ddd.chapter;
 
 public class UserApplicationService {
+
     private final IUserRepository userRepository;
     private final UserService userService;
 
     public UserApplicationService(IUserRepository userRepository, UserService userService) {
         this.userRepository = userRepository;
         this.userService = userService;
+    }
+
+    public UserData get(String userId){
+        var targetId = new UserId(userId);
+        var user = userRepository.find(targetId);
+
+        if(user == null){return null;}
+
+        return new UserData(user);
     }
 
     public void register(String name){
@@ -17,8 +27,18 @@ public class UserApplicationService {
         userRepository.save(user);
     }
 
-    public User get(String userId){
+    public void update(String userId, String name){
         var targetId = new UserId(userId);
-        return userRepository.find(targetId);
+        var user = userRepository.find(targetId);
+        if(user == null){System.out.println("User does not exist");}
+
+        var newUserName = new UserName(name);
+        user.changeName(newUserName);
+        if(userService.exist(user)){
+            System.out.println("User already exists");
+        }
+        userRepository.save(user);
     }
+
+
 }
